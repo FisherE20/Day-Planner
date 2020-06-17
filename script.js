ca430361b8b2ed4eb0cc9bcf3d19518df19e510e
Erin Fisher <flybirdie85@gmail.com>
2020-06-16 17:02:45 -0700

    change HTML to add a textarea, JS .on('click') works but doesn't save to local, data is being inputed on all hours not just 1.

--- script.js
+++ script.js
@@ -1,81 +1,96 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 $(document).ready(function(){
+$(".hour").on('click', function(){
+    var input= prompt();
+    $('.description').append(input);
+});
 
 
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-$(function (){
-    $('#schedulerContainer').dxScheduler({
-        dataSource: data,
-        currentData:new Date(2020/06/11),
-        startDayHour: 8,
-        endDayHour: 19,
-        views: ["day"],
-        currentView: "day",
-        resource: [{
-            dataSource: resourcsData,
-            allowMultiple: true,
-            field: "ownerId",
-            label: "Owner"
-        }]   
-    });
 
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-$("#descrip").on('click', function(){
-    $("div#form1").append(
-        // Creating Form Div and Adding <h2> and <p> Paragraph Tag in it.
-        $("<h3/>").text("Contact Form"), $("<p/>").text("This is my form. Please fill it out. It's awesome!"), $("<form/>", {
-        action: '#',
-        method: '#'
-        }).append(
-        // Create <form> Tag and Appending in HTML id="descrip".
-        $("<input/>", {
-        type: 'text',
-        id: 'vevent',
-        name: 'event',
-        placeholder: 'Event'
-        }), // Creating Input Element With Attribute.
-        $("<input/>", {
-        type: 'text',
-        id: 'vtime',
-        name: 'time',
-        placeholder: 'Start Time'
-        }), $("<textarea/>", {
-        rows: '5px',
-        cols: '27px',
-        type: 'text',
-        id: 'vdes',
-        name: 'description',
-        placeholder: 'description'
-        }), $("<br/>"), $("<input/>", {
-        type: 'submit',
-        id: 'submit',
-        value: 'Submit'
-        })))
-        });
+$(".saveBtn").on('click', function(){
+    //Working- you are alerted!
+    alert('You clicked the Save Button')
+    //local storage states content is undefined
+    $('.description').html("description");
+     localStorage.content = $('description').html();
+     $('.description').html(localStorage.content)
+})
+
+
+// // THEN the current day is displayed at the top of the calendar
+// // WHEN I scroll down
+// // THEN I am presented with timeblocks for standard business hours
+// $(function (){
+//     $('#schedulerContainer').dxScheduler({
+//         dataSource: data,
+//         currentData:new Date(2020/06/11),
+//         startDayHour: 8,
+//         endDayHour: 19,
+//         views: ["day"],
+//         currentView: "day",
+//         resource: [{
+//             dataSource: resourcsData,
+//             allowMultiple: true,
+//             field: "ownerId",
+//             label: "Owner"
+//         }]   
+//     });
+
+// // WHEN I view the timeblocks for that day
+// // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// // WHEN I click into a timeblock
+// $("#descrip").on('click', function(){
+//     $("div#form1").append(
+//         // Creating Form Div and Adding <h2> and <p> Paragraph Tag in it.
+//         $("<h3/>").text("Contact Form"), $("<p/>").text("This is my form. Please fill it out. It's awesome!"), $("<form/>", {
+//         action: '#',
+//         method: '#'
+//         }).append(
+//         // Create <form> Tag and Appending in HTML id="descrip".
+//         $("<input/>", {
+//         type: 'text',
+//         id: 'vevent',
+//         name: 'event',
+//         placeholder: 'Event'
+//         }), // Creating Input Element With Attribute.
+//         $("<input/>", {
+//         type: 'text',
+//         id: 'vtime',
+//         name: 'time',
+//         placeholder: 'Start Time'
+//         }), $("<textarea/>", {
+//         rows: '5px',
+//         cols: '27px',
+//         type: 'text',
+//         id: 'vdes',
+//         name: 'description',
+//         placeholder: 'description'
+//         }), $("<br/>"), $("<input/>", {
+//         type: 'submit',
+//         id: 'submit',
+//         value: 'Submit'
+//         })))
+//         });
  
 
-});
+// });
 
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
+// // WHEN I click the save button for that timeblock
+// // THEN the text for that event is saved in local storage
+// // WHEN I refresh the page
+// // THEN the saved events persist
+// // ```
 
-// THEN I can enter an event
+// // THEN I can enter an event
   
-// WHEN I click the save button for that timeblock
-$(".saveBtn").on("click", function(event){
-    // THEN the text for that event is saved in local storage
-    $('#test').html("Test");
-    localStorage.content = $('#test').html();
-    $('#test').html(localStorage.content)
-});
+// // WHEN I click the save button for that timeblock
+// $(".saveBtn").on("click", function(event){
+//     // THEN the text for that event is saved in local storage
+//     $('#test').html("Test");
+//     localStorage.content = $('#test').html();
+//     $('#test').html(localStorage.content)
+// });
 
 // WHEN I refresh the page
 // THEN the saved events persist
